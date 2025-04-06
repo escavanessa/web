5487f6b5f62f01b20749c769e4907501bf1c670f
vanessa <escavanessa@gmail.com>
2025-04-06 12:54:42 -0400

    figuring onut windows 95. going to try fitting react95 into this

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import './Info.css'
 import "bootstrap-icons/font/bootstrap-icons.css";
 
+
 function Info(props) {
   return (
     <>
     <div id='info-container'>
 
       <div id='info-title'>
-        <i id='info-exit' class="bi bi-x-square" onClick={props.hideInfo}></i>
-        info.exe
+        <p className='info-title'>Info.exe</p>
+        <button id='info-exitbutton' onClick={props.hideInfo}>X</button>
       </div>
-      <div id='info-context'>
-         click the icons for more information!
-      </div>
-      <div id='info-inner'>
+      <div id='info-body'>
+        <p>Click the icons for more information!</p>
           <i id='info-icon' class="bi bi-easel2"></i>
           <i id='info-icon' class="bi bi-journal"></i>
           <i id='info-icon' class="bi bi-bandaid"></i>
           <i id='info-icon' class="bi bi-code-square"></i>
-
       </div>
     </div>
     
